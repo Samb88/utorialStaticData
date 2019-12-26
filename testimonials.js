a0bbd25005872b35efa6a0df8952c128ec00c5ce
Sam Ben - Yakar <sam@wescover.com>
2019-12-26 22:40:16 +0200

    Add more files

---
+++ testimonials.js
@@ -0,0 +1,24 @@
+[{
+                            "id": 1,
+                            "name": "Mia Mabanta",
+                            "avatar": "/images/user-1.jpg",
+                            "profile": "/images/client-1.png",
+                            "designation": "Youtuber",
+                            "body": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
+                        },
+                        {
+                            "id": 2,
+                            "name": "Emmy Loren",
+                            "avatar": "/images/user-2.jpg",
+                            "profile": "/images/client-2.png",
+                            "designation": "Instagram Influencer",
+                            "body": "Sed consequat lobortis risus, vitae congue nulla tempor id. Curabitur eu augue id nibh tristique tristique. Phasellus vel est nisi"
+                        },
+                        {
+                            "id": 3,
+                            "name": "Astell Mercell",
+                            "avatar": "/images/user-3.jpg",
+                            "profile": "/images/client-3.png",
+                            "designation": "Makeup artist",
+                            "body": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
+                        }]
---
+++ todo.js
@@ -0,0 +1,38 @@
+[
+      {
+         "id": 1,
+         "title": "Arrange meeting for Vuely update",
+         "date": "Today 10:35 | By Admin",
+         "completed": true
+      },
+      {
+         "id": 2,
+         "title": "Create new pages for theme",
+         "date": "Today 10:35 | By Admin",
+         "completed": false
+      },
+      {
+         "id": 3,
+         "title": "Prepare launch image for Lawking",
+         "date": "Today 10:35 | By Admin",
+         "completed": false
+      },
+      {
+         "id": 4,
+         "title": "Pay internet bill",
+         "date": "Today 10:35 | By Admin",
+         "completed": false
+      },
+      {
+         "id": 5,
+         "title": "Generate salary reports",
+         "date": "Today 10:35 | By Admin",
+         "completed": false
+      },
+      {
+         "id": 6,
+         "title": "Update vuely dashboard",
+         "date": "Today 10:35 | By Admin",
+         "completed": false
+      }
+   ]
