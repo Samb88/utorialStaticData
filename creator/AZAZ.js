f9f4e4d18de20843560b868f2daa1b3f641cbf1c
Sam Ben - Yakar <sam@wescover.com>
2020-01-13 00:24:38 +0400

    Add creator

---
+++ creator/AZAZ.js
@@ -0,0 +1,32 @@
+[
+  {
+    "title": "Shipping not arrived",
+    "date": "4 Jan 2017, 05:00PM",
+    "content": "Opened by Timothy Owens today",
+    "status": false
+  },
+  {
+    "title": "Product not good",
+    "date": "4 Jan 2017, 05:00PM",
+    "content": "Opened by Timothy Owens today",
+    "status": true
+  },
+  {
+    "title": "Payment failed",
+    "date": "4 Jan 2017, 05:00PM",
+    "content": "Opened by Timothy Owens today",
+    "status": false
+  },
+  {
+    "title": "Credit card not accepting",
+    "date": "4 Jan 2017, 05:00PM",
+    "content": "Opened by Timothy Owens today",
+    "status": true
+  },
+  {
+    "title": "Unable to add credit card ",
+    "date": "4 Jan 2017, 05:00PM",
+    "content": "Opened by Timothy Owens today",
+    "status": true
+  }
+]
